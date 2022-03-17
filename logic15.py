85cffcce96c7a29af9edf4d692e88c888123db8e
Umidjon <mrumidjon2020@gmail.com>
2022-03-17 23:45:22 +0500

    All digits sum is odd

--- logic15.py
+++ logic15.py
@@ -1,9 +1,16 @@
 def main(a):
     """
-    Given a three-digit integer a,  check the following statement "All digits sum is odd".
+    Given a three-digit integer a,  check the following statement "All digits sum is odd""All digits sum is odd".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    sum = 0
+    sum += a % 10
+    a /= 10
+
+    sum += a % 10
+    a /= 10
+
+    return sum % 2 == 1
