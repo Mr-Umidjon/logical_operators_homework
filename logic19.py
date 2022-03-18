12844d7511dac7318ffa4bc0051205e6983073d6
Umidjon <mrumidjon2020@gmail.com>
2022-03-18 18:39:56 +0500

    palindrome

--- logic19.py
+++ logic19.py
@@ -16,7 +16,7 @@ def main(x):
 
     y3 = x % 10
     x //= 10
-    return (y1 * 1000 + y2 * 10 + y3 == y3 * 1000 + y2 * 10 + y1) and y1 > 0 or (
+    return (y1 * 100 + y2 * 10 + y3 == y3 * 100 + y2 * 10 + y1) and y1 > 0 or (
             y1 * 10 + y2 == y2 * 10 + y1)
 
 
