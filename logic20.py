57b2dc42994987657264adc679faae39c9c1bc2c
Umidjon <mrumidjon2020@gmail.com>
2022-03-18 18:05:08 +0500

    number of ones is greater than zero

--- logic20.py
+++ logic20.py
@@ -8,5 +8,29 @@ def main(n):
     Returns:
         bool: answer
     """
+    s = 0
+    n1 = n
+    x1 = n % 10
+    n //= 10
+    s += x1
 
-    return
+    x2 = n % 10
+    n //= 10
+    s += x2
+
+    x3 = n % 10
+    n //= 10
+    s += x3
+
+    x4 = n % 10
+    n //= 10
+    s += x4
+
+    x5 = n % 10
+    n //= 10
+    s += x5
+
+    return 10000 <= n1 <= 99999 and s >= 3 or 1000 <= n1 <= 9999 and s >= 3 or 100 <= n1 <= 999 and s >= 2 or 10 <= n1 <= 99 and s >= 2 or s == 1 and 1 <= n1 <= 9
+
+
+print(main(110))
